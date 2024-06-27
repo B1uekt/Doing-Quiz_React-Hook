6a892d4c4958e99cf531ab75141974b4250165bb
khanhthi <khanhthi0211@gmail.com>
2024-06-27 21:42:15 +0700

    state in react

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
 class MyComponents extends React.Component {
+    state = {
+        name: 'Zeust',
+        age: 21
+    }
+
+    handleOnChangeName(event) {
+        this.setState({
+            name: event.target.value
+        })
+
+    }
     render() {
-        let name = 'Zeusttttttt';
 
         return (
-            <div>
-                hello my component, My name is {name}
-            </div>
+            <>
+                <div className="first">
+                    <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)} />
+
+                    My name is {this.state.name}
+                </div>
+                <div className="second">
+                    Age: {this.state.age}
+                </div>
+            </>
         )
     }
 }
