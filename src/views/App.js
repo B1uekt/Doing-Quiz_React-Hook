de2192d4094ad04219b878454deb4deab46de3d8
khanhthi <khanhthi0211@gmail.com>
2024-06-27 20:09:42 +0700

    Create first component

--- src/views/App.js
+++ src/views/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './Example/MyComponents'
+import MyComponents from './Example/MyComponents';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponents />
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponents.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MyComponents extends React.Component {
+    render() {
+        let name = 'Zeusttttttt';
+
+        return (
+            <div>
+                hello my component, My name is {name}
+            </div>
+        )
+    }
+}
+
+export default MyComponents;
