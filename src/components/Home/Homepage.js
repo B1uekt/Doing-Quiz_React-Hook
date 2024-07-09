3a3f0631a0ca203a024e621dc12f721221709256
khanhthi <khanhthi0211@gmail.com>
2024-07-10 00:21:30 +0700

    Design Home Page

---
+++ src/components/Home/Homepage.js
@@ -0,0 +1,18 @@
+import videoHomePage from '../../assets/audio/video-homepage.mp4'
+const HomePage = (props) => {
+    return (
+        <div className="homepage-container">
+            <video autoPlay muted loop>
+                <source src={videoHomePage} />
+            </video>
+            <div className="homepage-content">
+                <div className='main-title'>There's a better way to ask</div>
+                <div className='title'>You don't want to make boring form. and your audience won't answer one. Create a typeform instead- and make everyone happy.</div>
+                <div class='title-btn'>
+                    <button>Get's started.It's free</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default HomePage
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import User from './components/User/User'
 import Admin from './components/Admin/Admin'
+import HomePage from './components/Home/Homepage';
 
 ReactDOM.render(
 
@@ -14,6 +15,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
+          <Route index element={<HomePage />}></Route>
           <Route path="users" element={<User />}></Route>
           <Route path="admins" element={<Admin />}></Route>
         </Route>
