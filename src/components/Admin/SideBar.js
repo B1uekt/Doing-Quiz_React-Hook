5b4e072486bf14745a2b22e714da7682c526a4ec
khanhthi <khanhthi0211@gmail.com>
2024-07-10 23:20:35 +0700

    Design Admin sidebar

---
+++ src/components/Admin/SideBar.js
@@ -0,0 +1,73 @@
+import 'react-pro-sidebar/dist/css/styles.css';
+import {
+    ProSidebar,
+    Menu,
+    MenuItem,
+    SubMenu,
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
+} from 'react-pro-sidebar';
+
+import { FaTachometerAlt, FaGem, FaReact, FaRegLaughWink } from 'react-icons/fa';
+import sidebarBg from '../../assets/images/bg2.jpg'
+const SideBar = (props) => {
+    const { image, collapsed, toggled, handleToggleSidebar } = props;
+    return (
+        <>
+            <ProSidebar
+                image={sidebarBg}
+                collapsed={collapsed}
+                toggled={toggled}
+                breakPoint="md"
+                onToggle={handleToggleSidebar}
+            >
+                <SidebarHeader>
+                    <div
+                        style={{
+                            padding: '24px',
+                            textTransform: 'uppercase',
+                            fontWeight: 'bold',
+                            fontSize: 14,
+                            letterSpacing: '1px',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        <FaReact size={'3em'} color={'00bfff'} />
+                        <span>Admin </span>
+                    </div>
+                </SidebarHeader>
+
+                <SidebarContent>
+                    <Menu iconShape="circle">
+                        <MenuItem
+                            icon={<FaTachometerAlt />}
+                        // suffix={<span className="badge red">New</span>}
+                        >
+                            Dashboard
+                        </MenuItem>
+                        {/* <MenuItem icon={<FaGem />}> components </MenuItem> */}
+                    </Menu>
+                    <Menu iconShape="FaGem">
+                        <SubMenu
+                            suffix={<span className="badge yellow">3</span>}
+                            icon={<FaRegLaughWink />}
+                            title="Features"
+                        >
+                            <MenuItem>Quản lý Users</MenuItem>
+                            <MenuItem>Quản lý bài Quiz</MenuItem>
+                            <MenuItem>Quản lý câu hỏi</MenuItem>
+                        </SubMenu>
+
+                    </Menu>
+                </SidebarContent>
+
+
+            </ProSidebar>
+        </>
+    )
+}
+
+export default SideBar;
