3c6349bc9ac2080f45630551bd20f4d34f16bd96
khanhthi <khanhthi0211@gmail.com>
2024-07-11 14:54:04 +0700

    Get image preview before uploading

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,20 +1,35 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-
+import Pic from '../../../assets/images/test.jpg'
 const ModalCreateUser = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [role, setRole] = useState('USER');
+    const [image, setImage] = useState('');
+    const [previewImg, setPreviewImg] = useState('')
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handleUploadImg = (event) => {
+        if (event.target && event.target.files && event.target.files[0]) {
+            setPreviewImg(URL.createObjectURL(event.target.files[0]))
+            setImage(event.target.files[0])
+        }
+        else {
+            setPreviewImg("")
+        }
 
+    }
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
                 Add new user
             </Button>
 
-            <Modal backdrop="static" show={show} onHide={handleClose} size="xl">
+            <Modal backdrop="static" show={show} onHide={handleClose} size="xl" className='modal-add-user'>
                 <Modal.Header closeButton>
                     <Modal.Title>Add New User</Modal.Title>
                 </Modal.Header>
@@ -22,19 +37,19 @@ const ModalCreateUser = () => {
                     <form className="row g-3">
                         <div className="col-md-6">
                             <label className="form-label">Email</label>
-                            <input type="email" className="form-control" />
+                            <input type="email" className="form-control" value={email} onChange={(event) => setEmail(event.target.value)} />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">Password</label>
-                            <input type="password" className="form-control" />
+                            <input type="password" className="form-control" value={password} onChange={(event) => setPassword(event.target.value)} />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">Username</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" value={username} onChange={(event) => setUsername(event.target.value)} />
                         </div>
                         <div className="col-md-4">
                             <label className="form-label">Role</label>
-                            <select id="inputState" className="form-select">
+                            <select id="inputState" className="form-select" onChange={(event) => setRole(event.target.value)} value={role}>
                                 <option selected value="USER">USER</option>
                                 <option value="ADMIN">ADMIN</option>
                             </select>
@@ -42,8 +57,18 @@ const ModalCreateUser = () => {
                         <div className='col-md-12'>
                             <label className="form-label">
                                 Image
-                                <input class="form-control" type="file" id="formFile"></input>
+                                <input class="form-control" type="file" id="formFile" onChange={(event) => handleUploadImg(event)}></input>
                             </label>
+                        </div>
+                        <div className='col-md-12 img-preview'>
+                            {previewImg ?
+
+                                <img src={previewImg} />
+                                :
+                                <span>Preview Image</span>
+                            }
+
+
                         </div>
                     </form></Modal.Body>
                 <Modal.Footer>
