294f6f873d16d20900f28d46dd8423aa1085a14f
khanhthi <khanhthi0211@gmail.com>
2024-08-07 23:18:43 +0700

    update header admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -7,9 +7,30 @@ import PerfectScrollbar from 'react-perfect-scrollbar'
 import Language from '../../components/Header/Language';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useTranslation } from 'react-i18next';
+import Profile from '../Header/Profile';
+import { postLogOut } from '../../services/AuthServices';
+import { toast } from 'react-toastify';
+import { doLogout } from '../../redux/action/userAction';
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
 const Admin = (props) => {
     const { t } = useTranslation();
     const [collapsed, setCollapsed] = useState(false)
+    const navigate = useNavigate();
+
+    const account = useSelector(state => state.user.account)
+    const dispatch = useDispatch()
+    const [showProfile, setShowProfile] = useState(false);
+    const handleLogOut = async () => {
+        let res = await postLogOut(account.email, account.refresh_token)
+        if (res && res.EC === 0) {
+            dispatch(doLogout())
+            navigate('/login')
+        }
+        else {
+            toast.error(res.EM)
+        }
+    }
     return (
 
         <div className="admin-container">
@@ -22,8 +43,8 @@ const Admin = (props) => {
                     <div className="right-side d-flex">
                         <Language />
                         <NavDropdown className="nav-dropdown" title={t('Header.setting.title')} id="basic-nav-dropdown">
-                            <NavDropdown.Item >{t('Header.setting.profile')}</NavDropdown.Item>
-                            <NavDropdown.Item >{t('Header.setting.logout')}</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => setShowProfile(true)}>{t('Header.setting.profile')}</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => handleLogOut()}>{t('Header.setting.logout')}</NavDropdown.Item>
                         </NavDropdown>
                     </div>
 
@@ -36,7 +57,10 @@ const Admin = (props) => {
                 </div>
 
             </div>
-
+            <Profile
+                show={showProfile}
+                setShow={setShowProfile}
+            />
         </div>
 
 
